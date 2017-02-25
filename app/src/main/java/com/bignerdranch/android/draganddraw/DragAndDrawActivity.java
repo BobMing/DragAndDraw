00e08e627d420475e32262d3c73e3bb975815dd7
BobMing <bobmingxie@vip.qq.com>
2017-02-26 03:39:05 +0800

    Finish Chapter 29 : Custom Views and Touch Events
    完成：
    29.2：创建定制视图
    29.3：处理触摸事件
    29.4：onDraw(...)方法内的图形绘制

--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -1,7 +1,6 @@
 package com.bignerdranch.android.draganddraw;
 
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 
 public class DragAndDrawActivity extends SingleFragmentActivity {
 
